96d017aa92cc882a26d2a2d54a61776204413683
jungyu <jungyu3826@gmail.com>
2023-09-02 13:30:33 +0900

    [feat] create style theme provider

---
+++ src/styles/theme.ts
@@ -0,0 +1,25 @@
+import { DefaultTheme } from 'styled-components';
+
+const colors = {
+  //main color
+  primary1: '#028DE9',
+  primary2: '#007AD6',
+
+  //gray color
+  gray1: '#605F5F59',
+  gray2: '#605F5F',
+  gray3: '#414040',
+  gray4: '#201F1F',
+
+  //black color
+  black: '#000000',
+
+  //white color
+  white: '#FFFFFF',
+};
+
+export const TColorsTypes = typeof colors;
+
+export const theme: DefaultTheme = {
+  colors,
+};
