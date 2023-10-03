0e9d6c855adc68080aaf9eab8bcb269cbed31fb2
jungyu <jungyu3826@gmail.com>
2023-10-03 14:52:29 +0900

    [feat] modify MyDetailReputation page layout

--- src/styles/components/common/selector/reputQuestion.styles.ts
+++ src/styles/components/common/selector/reputQuestion.styles.ts
@@ -2,11 +2,14 @@ import styled from 'styled-components';
 
 export const QuestionContainer = styled.div`
   display: flex;
+  width: 1116px;
+  padding: 32px;
   flex-direction: column;
   align-items: flex-start;
   gap: 24px;
-  width: 1025px;
-  height: 92px;
+  border-radius: 16px;
+  background: ${({ theme }) => theme.colors.white};
+  box-shadow: 0px 3px 12px 0px rgba(0, 0, 0, 0.09);
 
   & h2 {
     width: 738px;
@@ -25,6 +28,11 @@ export const SelectContainer = styled.div`
   display: flex;
   align-items: flex-start;
   gap: 14px;
+
+  & :first-child,
+  & :last-child {
+    border: 1px solid ${({ theme }) => theme.colors.primary1};
+  }
 `;
 
 export const Select = styled.div<{ $isPrimary?: boolean }>`
@@ -33,6 +41,7 @@ export const Select = styled.div<{ $isPrimary?: boolean }>`
   justify-content: center;
   align-items: center;
   gap: 12px;
+  cursor: pointer;
 
   border-radius: 8px;
   background: ${({ theme }) => theme.colors.grey5};
--- src/styles/page/reput/myDetailReputation.styles.ts
+++ src/styles/page/reput/myDetailReputation.styles.ts
@@ -5,38 +5,51 @@ export const Wrapper = styled.div`
   flex-direction: column;
   justify-content: center;
   width: 100%;
+  gap: 80px;
+  align-items: center;
+  padding-top: 90px;
+  padding-bottom: 177px;
+  background-color: ${({ theme }) => theme.colors.grey5};
+
+  & header {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 50px;
+  }
 
   & main {
-    display: inline-flex;
+    display: flex;
     flex-direction: column;
-    align-items: flex-start;
-    gap: 63px;
+    align-items: center;
+    gap: 50px;
   }
 `;
 
 export const HeaderContainer = styled.div`
-  display: inline-flex;
-  align-items: center;
-  gap: 8px;
-  margin-top: 74px;
-  margin-left: 162px;
-  & h1 {
-    color: ${({ theme }) => theme.colors.grey2};
-    font: ${({ theme }) => theme.fonts.headline2};
-    letter-spacing: -0.48px;
-    line-height: 35.04px;
-  }
-`;
-
-export const WriterInforContainer = styled.div`
-  display: inline-flex;
+  display: flex;
   flex-direction: column;
   align-items: flex-start;
-  gap: 32px;
-  margin-left: 162px;
-  margin-top: 66px;
+  gap: 50px;
 
-  & h2 {
+  & section {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 50px;
+  }
+
+  & p {
+    display: flex;
+    width: 1116px;
+    padding: 20px 10px;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    border-radius: 8px;
+    background: ${({ theme }) => theme.colors.white};
+    box-shadow: 0px 3px 12px 0px rgba(0, 0, 0, 0.09);
+
     color: ${({ theme }) => theme.colors.error};
     text-align: center;
     font: ${({ theme }) => theme.fonts.body};
@@ -44,32 +57,78 @@ export const WriterInforContainer = styled.div`
   }
 `;
 
-export const Information = styled.div`
+export const Header = styled.div`
   display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: flex-start;
-  gap: 31px;
-`;
-
-export const Line = styled.div`
-  width: 1116px;
-  height: 1px;
-  background: rgba(96, 95, 95, 0.35);
-`;
-
-export const Badge = styled.div`
-  display: flex;
-  padding: 10px;
-  justify-content: center;
   align-items: center;
-  gap: 10px;
-  border-radius: 8px;
-  background: ${({ theme }) => theme.colors.primary1};
+  gap: 590px;
 
-  color: ${({ theme }) => theme.colors.white};
+  & h1 {
+    color: ${({ theme }) => theme.colors.grey3};
+    font: ${({ theme }) => theme.fonts.headline1};
+    letter-spacing: -0.68px;
+
+    & b {
+      color: ${({ theme }) => theme.colors.primary1};
+    }
+  }
+`;
+
+export const Date = styled.div`
+  display: flex;
+  align-items: flex-end;
+  gap: 8px;
+  height: 24px;
+  color: ${({ theme }) => theme.colors.grey2};
   font: ${({ theme }) => theme.fonts.subtitle1};
   letter-spacing: -0.4px;
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  align-items: flex-start;
+  gap: 20px;
+`;
+
+export const Button = styled.div`
+  display: flex;
+  padding: 16px 20px;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+  border-radius: 8px;
+  background: ${({ theme }) => theme.colors.primary1};
+  width: 177px;
+  height: 64px;
+  cursor: pointer;
+
+  color: ${({ theme }) => theme.colors.white};
+  font: ${({ theme }) => theme.fonts.subtitle2};
+  letter-spacing: -0.36px;
+`;
+
+export const WriterInforContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 24px;
+
+  & h1 {
+    color: ${({ theme }) => theme.colors.grey4};
+    font: ${({ theme }) => theme.fonts.headline2};
+    letter-spacing: -0.48px;
+  }
+`;
+
+export const Information = styled.div`
+  display: flex;
+  width: 1116px;
+  padding: 32px;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 10px;
+  border-radius: 8px;
+  background: ${({ theme }) => theme.colors.white};
+  box-shadow: 0px 3px 12px 0px rgba(0, 0, 0, 0.09);
 `;
 
 export const InforTable = styled.div`
@@ -95,22 +154,12 @@ export const InforItem = styled.div`
   }
 `;
 
-export const ReputConatainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  gap: 60px;
-  padding: 0 162px;
-`;
-
 export const DisclosureContainer = styled.div`
-  display: inline-flex;
+  display: flex;
+  height: 211px;
   flex-direction: column;
   align-items: flex-start;
   gap: 40px;
-  height: 211px;
-
-  // margin-left: 162px;
 
   & > div {
     display: flex;
@@ -141,7 +190,9 @@ export const Disclosure = styled.div`
     align-items: center;
     gap: 10px;
     border-radius: 16px;
-    background: ${({ theme }) => theme.colors.grey5};
+    background: ${({ theme }) => theme.colors.white};
+
+    box-shadow: 0px 3px 12px 0px rgba(0, 0, 0, 0.09);
 
     color: ${({ theme }) => theme.colors.grey3};
     font: ${({ theme }) => theme.fonts.body};
@@ -153,18 +204,26 @@ export const CheckBox = styled.div`
   display: flex;
   align-items: center;
   gap: 8px;
-
+  height: 32px;
+  cursor: pointer;
   color: ${({ theme }) => theme.colors.grey3};
   font: ${({ theme }) => theme.fonts.subtitle2};
   letter-spacing: -0.36px;
+
+  & svg {
+    display: flex;
+    width: 24px;
+    height: 24px;
+    justify-content: center;
+    align-items: center;
+  }
 `;
 
-export const NondisclosureContainer = styled.div`
+export const NonDisclosureContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
-  gap: 40px;
-  margin-bottom: 91px;
+  gap: 24px;
   & h1 {
     width: 738px;
     color: ${({ theme }) => theme.colors.grey3};
