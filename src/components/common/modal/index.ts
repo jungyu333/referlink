02bfd1820248ce24329d95e038a4f2b7df6be4c2
jungyu <jungyu3826@gmail.com>
2023-10-05 21:39:23 +0900

    [feat] create CompleteConfirmModal component

--- src/components/common/modal/index.ts
+++ src/components/common/modal/index.ts
@@ -1,1 +1,2 @@
 export * from './ModifyConfirmModal';
+export * from './CompleteConfirmModal';
--- src/page/Landing.tsx
+++ src/page/Landing.tsx
@@ -1,14 +1,7 @@
-import { ModifyConfirmModal } from '@components/common';
 import * as S from '@styles/page/landing.styles';
 import { useState } from 'react';
 
 export const Landing = () => {
-  const [modalIsOpen, setModalIsOpen] = useState<boolean>(false);
-
-  const handleConfirm = () => {
-    console.log('확인');
-    setModalIsOpen(false);
-  };
   return (
     <S.Wrapper>
       <S.MainText>
@@ -17,15 +10,9 @@ export const Landing = () => {
         평판조회의 시작, 레퍼링크
       </S.MainText>
 
-      <S.Button onClick={() => setModalIsOpen(true)}>
+      <S.Button>
         <div>평판 작성하기</div>
       </S.Button>
-
-      <ModifyConfirmModal
-        isOpen={modalIsOpen}
-        onClose={() => setModalIsOpen(false)}
-        onConfirm={handleConfirm}
-      />
     </S.Wrapper>
   );
 };
