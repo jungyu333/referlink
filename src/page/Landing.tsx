d42311c708e249f9fc0acb81e977d6e4e497da94
jungyu <jungyu3826@gmail.com>
2023-10-10 13:06:18 +0900

    [feat] modify landing page layout

--- src/page/Landing.tsx
+++ src/page/Landing.tsx
@@ -1,17 +1,24 @@
+import { Button } from '@components/common/button';
 import * as S from '@styles/page/landing.styles';
 
 export const Landing = () => {
   return (
     <S.Wrapper>
-      <S.MainText>
-        누구나 평판 관리를 할 수 있게
-        <br />
-        평판조회의 시작, 레퍼링크
-      </S.MainText>
+      <main>
+        <S.MainText>
+          누구나 평판 관리를 할 수 있게
+          <br />
+          평판조회의 시작, 레퍼링크
+        </S.MainText>
 
-      <S.Button>
-        <div>평판 작성하기</div>
-      </S.Button>
+        <Button
+          buttonText="평판 작성 요청하기"
+          py="24px"
+          width="225px"
+          height="68px"
+          px="36px"
+        />
+      </main>
     </S.Wrapper>
   );
 };
