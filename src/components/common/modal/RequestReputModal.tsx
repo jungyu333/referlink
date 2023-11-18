f72db0a65f528cf2a1f34c7af68304d9a2ddf82c
jungyu <jungyu3826@gmail.com>
2023-11-18 16:53:15 +0900

    [feat] add TextInput Component Migration in RequestReputModal

--- src/components/common/modal/RequestReputModal.tsx
+++ src/components/common/modal/RequestReputModal.tsx
@@ -3,10 +3,15 @@ import * as S from '@styles/components/common/modal/requestReputModal.styles';
 import { ReactComponent as PhxSVG } from '@styles/images/svg/ph_x.svg';
 import { ReactComponent as FrameSVG } from '@styles/images/svg/Frame 36767.svg';
 import { useForm } from 'react-hook-form';
-import { TextInput } from '@components/common/input';
 import { IRequestReputationFormData } from '_types/reput';
 import { emailRegex } from '@constant/regex';
-import { Button, ButtonTypes, EmailInput, Fonts } from 'referlink-ui';
+import {
+  Button,
+  ButtonTypes,
+  EmailInput,
+  Fonts,
+  TextInput,
+} from 'referlink-ui';
 
 type Props = {
   isOpen: boolean;
