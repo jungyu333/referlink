7861cf0cfd68fd7603012cbf3bfc97bd56ca5bb7
jungyu <jungyu3826@gmail.com>
2023-11-18 18:22:58 +0900

    [feat] remove useless code in Landing Page

--- src/page/Landing.tsx
+++ src/page/Landing.tsx
@@ -1,14 +1,7 @@
 import * as S from '@styles/page/landing.styles';
 import { Button } from 'referlink-ui';
-import { useSwitch } from '../hooks/useSwitch';
-import {
-  ConfirmModal,
-  EmailConfirmModal,
-  RequestReputModal,
-} from '../components';
 
 export const Landing = () => {
-  const [isOpen, onOpen, onClose] = useSwitch();
   return (
     <S.Wrapper>
       <main>
@@ -24,17 +17,6 @@ export const Landing = () => {
           width="225px"
           height="68px"
           px="36px"
-          onClick={onOpen}
-        />
-
-        <RequestReputModal
-          isOpen={isOpen}
-          onClose={onClose}
-          //confirmLabel="dd"
-          //cancelLable="dd"
-          onConfirm={onClose}
-          //mainText="dd"
-          //subText="dd"
         />
       </main>
     </S.Wrapper>
