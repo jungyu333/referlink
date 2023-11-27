5b92b2f55f1f77fa61c2efa8402cc2c380d9e56e
jungyu <jungyu3826@gmail.com>
2023-11-27 15:33:07 +0900

    [feat] modify Signin Page ui

--- src/styles/page/auth/signIn.styles.ts
+++ src/styles/page/auth/signIn.styles.ts
@@ -1,38 +1,30 @@
 import { styled } from 'styled-components';
 
 export const Wrapper = styled.div`
+  background-color: ${({ theme }) => theme.colors.grey5};
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+export const Main = styled.div`
   display: inline-flex;
+  padding: 100px;
   flex-direction: column;
   align-items: center;
   gap: 24px;
-  margin-top: 130px;
-  margin-bottom: 138px;
-
-  & h2 {
-    color: ${({ theme }) => theme.colors.grey2};
-    font: ${({ theme }) => theme.fonts.caption};
-    letter-spacing: -0.32px;
-    font-size: 16px;
-
-    & b {
-      color: ${({ theme }) => theme.colors.primary1};
-      font: ${({ theme }) => theme.fonts.body2};
-      line-height: 150%;
-      letter-spacing: -0.32px;
-      text-decoration-line: underline;
-      cursor: pointer;
-    }
-  }
+  border-radius: 16px;
+  background: ${({ theme }) => theme.colors.white};
+  box-shadow: 0px 3px 40px 0px rgba(0, 0, 0, 0.05);
+  margin-top: 87px;
+  margin-bottom: 135px;
 `;
 
-export const SignInForm = styled.div`
+export const Header = styled.div`
   display: flex;
-
   flex-direction: column;
   align-items: center;
-  gap: 64px;
-
-  background-color: ${({ theme }) => theme.colors.white};
+  gap: 8px;
 
   & h1 {
     color: ${({ theme }) => theme.colors.grey4};
@@ -41,19 +33,25 @@ export const SignInForm = styled.div`
   }
 `;
 
-export const AuthForm = styled.form`
+export const SignInForm = styled.form`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
-  gap: 20px;
-  width: 400px;
+  gap: 40px;
 `;
 
 export const InputContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
-  gap: 16px;
+  gap: 8px;
+`;
+
+export const FindPassword = styled.div`
+  color: ${({ theme }) => theme.colors.primary2};
+  font: ${({ theme }) => theme.fonts.body};
+  letter-spacing: -0.32px;
+  cursor: pointer;
 `;
 
 export const ButtonContainer = styled.div`
@@ -61,43 +59,4 @@ export const ButtonContainer = styled.div`
   flex-direction: column;
   align-items: flex-start;
   gap: 40px;
-`;
-
-export const HelpText = styled.div`
-  color: ${({ theme }) => theme.colors.primary2};
-  font: ${({ theme }) => theme.fonts.body};
-  letter-spacing: -0.32px;
-  height: 42px;
-  width: 155px;
-  cursor: pointer;
-`;
-
-export const LineText = styled.div`
-  display: flex;
-  width: 400px;
-  justify-content: space-between;
-  align-items: center;
-  color: ${({ theme }) => theme.colors.grey3};
-  font: ${({ theme }) => theme.fonts.body};
-  letter-spacing: -0.32px;
-  & div {
-    width: 113px;
-    height: 1px;
-    background: rgba(96, 95, 95, 0.35);
-  }
-`;
-
-export const OnGoogle = styled.button`
-  display: flex;
-  padding: 13px 118px 13px 24px;
-  align-items: center;
-  gap: 94px;
-  border-radius: 8px;
-  border: 1px solid ${({ theme }) => theme.colors.grey1};
-  width: 400px;
-  color: ${({ theme }) => theme.colors.grey3};
-  background-color: ${({ theme }) => theme.colors.white};
-  cursor: pointer;
-  font: ${({ theme }) => theme.fonts.subtitle2};
-  letter-spacing: -0.36px;
 `;
