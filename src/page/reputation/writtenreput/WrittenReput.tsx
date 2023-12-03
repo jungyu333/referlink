84b9bcf764dcd163c9e6268927bce87bfcd27806
jungyu <jungyu3826@gmail.com>
2023-12-03 16:34:29 +0900

    [feat] add WrittenPage Header ui

--- src/page/reputation/writtenreput/WrittenReput.tsx
+++ src/page/reputation/writtenreput/WrittenReput.tsx
@@ -1,3 +1,48 @@
+import { Button, Fonts, svgNote } from 'referlink-ui';
+import styled from 'styled-components';
+
 export const WrittenReput = () => {
-  return <>WrittenReput</>;
+  return (
+    <Wrapper>
+      <Header>
+        <h1>내가 작성한 평판</h1>
+
+        <Button
+          buttonText="초대하기"
+          isIcon
+          svgIcon={svgNote}
+          width="138px"
+          height="52px"
+          fontStyle={Fonts.subtitle2}
+          px="20px"
+          py="10px"
+        />
+      </Header>
+
+      <main></main>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  background: ${({ theme }) => theme.colors.grey5};
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  gap: 101px;
+  flex-direction: column;
+  padding: 0 162px;
+  padding-top: 103px;
+  padding-bottom: 157px;
+`;
+
+const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  & h1 {
+    color: ${({ theme }) => theme.colors.primary1};
+    font: ${({ theme }) => theme.fonts.headline1};
+    letter-spacing: -0.68px;
+  }
+`;
