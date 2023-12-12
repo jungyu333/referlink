d6a527ff3d36023c6cf453dd94a884f8f1a73e5f
jungyu <jungyu3826@gmail.com>
2023-12-12 13:20:31 +0900

    [fix] add ReactModal AppElement

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,14 @@ import { ThemeProvider } from 'styled-components';
 import { CustomToast } from '@components/common/toast';
 import { theme } from 'referlink-ui';
 import GlobalStyle from './styles/global';
+import ReactModal from 'react-modal';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
+
+ReactModal.setAppElement('#root');
+
 root.render(
   <>
     <ThemeProvider theme={theme}>
