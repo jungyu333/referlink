1cd45d2f89d1d5f5ac094078b23d848d2c8d2d1f
jungyu <jungyu3826@gmail.com>
2024-01-03 21:42:34 +0900

    [feat] add LoadingSpinner in WrittenReput Page

--- src/page/reputation/writtenreput/WrittenReput.tsx
+++ src/page/reputation/writtenreput/WrittenReput.tsx
@@ -1,10 +1,17 @@
-import { Button, Fonts, ReputCard, svgNote } from 'referlink-ui';
+import {
+  Button,
+  Fonts,
+  LoadingSpinner,
+  ReputCard,
+  svgNote,
+} from 'referlink-ui';
 import * as S from '@styles/page/reput/writtenReput.styles';
 import { useCustomToast } from '@hooks/useCustomToast';
 import { ToastBody } from '@components/common';
 import { useCopyClipBoard } from '@hooks/useCopyClipBoard';
 import { useCustomQuery } from '@hooks/useCustomQuery';
 import { getWrittenReviewList } from '@api/reputation';
+import { EmptyContent } from '@components/reput';
 
 export const WrittenReput = () => {
   const { info } = useCustomToast();
@@ -34,34 +41,46 @@ export const WrittenReput = () => {
   };
 
   return (
-    <S.Wrapper>
-      <S.Header>
-        <h1>내가 작성한 평판</h1>
+    <LoadingSpinner isLoading={isLoading}>
+      <>
+        {!isLoading && writtenReviewList && (
+          <>
+            {writtenReviewList.data.length > 0 ? (
+              <S.Wrapper>
+                <S.Header>
+                  <h1>내가 작성한 평판</h1>
 
-        <Button
-          buttonText="초대하기"
-          isIcon
-          svgIcon={svgNote}
-          width="138px"
-          height="52px"
-          fontStyle={Fonts.subtitle2}
-          px="20px"
-          py="10px"
-          onClick={onClickInvite}
-        />
-      </S.Header>
+                  <Button
+                    buttonText="초대하기"
+                    isIcon
+                    svgIcon={svgNote}
+                    width="138px"
+                    height="52px"
+                    fontStyle={Fonts.subtitle2}
+                    px="20px"
+                    py="10px"
+                    onClick={onClickInvite}
+                  />
+                </S.Header>
 
-      <S.Main>
-        {[1, 2, 3].map((item) => (
-          <ReputCard
-            mainContent="temp"
-            headContent="temp"
-            date="temp"
-            key={item}
-            onClick={() => console.log(item)}
-          />
-        ))}
-      </S.Main>
-    </S.Wrapper>
+                <S.Main>
+                  {[1, 2, 3].map((item) => (
+                    <ReputCard
+                      mainContent="temp"
+                      headContent="temp"
+                      date="temp"
+                      key={item}
+                      onClick={() => console.log(item)}
+                    />
+                  ))}
+                </S.Main>
+              </S.Wrapper>
+            ) : (
+              <EmptyContent />
+            )}
+          </>
+        )}
+      </>
+    </LoadingSpinner>
   );
 };
